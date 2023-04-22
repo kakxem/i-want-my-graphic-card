69f9a623d7c769f0a45b97d24173504e08c236b1
kakxem <paul.antonio.almasi@gmail.com>
2023-04-22 22:49:29 +0200

    feat: initial commit and scraper

---
+++ README.md
@@ -0,0 +1,32 @@
+# Scraper for get my next graphic card
+
+Yeah, I know, I'm a nerd. I'm a nerd that want to get a new graphic card, but I don't want to pay the price that the market is asking for. So I decided to make a scrapper to get the price of the graphic card that I want.
+
+P.S: That comment was made by Copilot, don't blame me.
+
+## How to use
+
+1. Clone the repository
+2. Install the dependencies
+3. Run the script
+4. Wait for the magic to happen
+
+### Clone the repository
+
+```bash
+git clone
+```
+
+### Install the dependencies
+
+```bash
+pnpm install
+```
+
+Replace `pnpm` with `npm` or `yarn` if you don't have `pnpm` installed.
+
+### Run the script
+
+```bash
+pnpm start
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const playwright = require('playwright')
+
+const PAGE = 'https://www.chollometro.com/categorias/tarjetas-graficas'
+const PRODUCT = '4090'
+const BELOW_PRICE = 1500
+const BROWSERS = ['chromium', 'firefox']
+const NUMBER_OF_PAGES = 10
+
+const scraper = async () => {
+  for (const browserType of BROWSERS) {
+    const browser = await playwright[browserType].launch()
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto(PAGE)
+
+    // accept cookies
+    await page.waitForSelector('button[data-t="acceptAllBtn"]')
+    const acceptCookies = await page.$('button[data-t="acceptAllBtn"]')
+    if (acceptCookies) {
+      await acceptCookies.click()
+    }
+
+    // Start scraping
+    for (let i = 0; i < NUMBER_OF_PAGES; i++) {
+      const elements = await page.$$('article')
+      let element = null
+      const info = []
+      for (const el of elements) {
+        const strong = await el.$('strong.thread-title')
+        if (strong) {
+          const text = await (await strong.getProperty('textContent')).jsonValue()
+          const stock = await el.$('span:has-text("Agotado")')
+          const price = await el.$('span.thread-price')
+          const priceText = await (await price.getProperty('textContent')).jsonValue()
+          const priceNumber = parseInt(priceText.replace('€', ''))
+
+          console.log({ text, stock: !!stock, price: priceText })
+          info.push({ text, stock: !!stock, price: priceText })
+          if (text.includes(PRODUCT) && !stock && priceNumber < BELOW_PRICE) {
+            element = el
+            break
+          }
+        }
+      }
+
+      // If we found the product, we stop the loop
+      if (element) {
+        const link = await element.$('a')
+        const href = await (await link.getProperty('href')).jsonValue()
+        console.log('Found in page', i + 1, href)
+        break
+      }
+      console.log('Not found in page', i + 1)
+
+      // Go to next page
+      const nextButton = await page.$('button[aria-label="Siguiente página"]')
+      if (nextButton) {
+        console.log('Next page')
+        await nextButton.click()
+        // wait until the page is loaded
+        await page.waitForSelector('article')
+      } else {
+        console.log('No more pages')
+        break
+      }
+    }
+
+    await browser.close()
+  }
+}
+
+scraper()
